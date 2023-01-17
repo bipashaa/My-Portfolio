0e72e6b59bd23eb182a112d7e40c1924489e9f7c
Bipasha Dutta <bipashaaduttaa@gmail.com>
2023-01-18 00:02:18 +0530

    Added skills progress bar

--- portfolio-site/src/components/Skills/Skills.jsx
+++ portfolio-site/src/components/Skills/Skills.jsx
@@ -6,12 +6,67 @@ export default function Skills() {
 		<div id="skills" className="container">
 			<h3>Skills.</h3>
 			<div>
-				<table class="skill">
-					<tr>
-						<td>HTML</td>
-						<LinearProgress variant="determinate" value={50} />
-					</tr>
-				</table>
+				<div class="skill">
+					<div class="progress-bar-container">
+						<div className="skill-name-percent">
+							<h2>HTML</h2>
+							<h6>99%</h6>
+						</div>
+						<div class="progress-bar">
+							<span class="percentage html"></span>
+						</div>
+					</div>
+
+					<div class="progress-bar-container">
+						<div className="skill-name-percent">
+							<h2>CSS</h2>
+							<h6>85%</h6>
+						</div>
+						<div class="progress-bar">
+							<span class="percentage css"></span>
+						</div>
+					</div>
+
+					<div class="progress-bar-container">
+						<div className="skill-name-percent">
+							<h2>JavaScript</h2>
+							<h6>80%</h6>
+						</div>
+						<div class="progress-bar">
+							<span class="percentage js"></span>
+						</div>
+					</div>
+
+					<div class="progress-bar-container">
+						<div className="skill-name-percent">
+							<h2>Java</h2>
+							<h6>85%</h6>
+						</div>
+						<div class="progress-bar">
+							<span class="percentage java"></span>
+						</div>
+					</div>
+
+					<div class="progress-bar-container">
+						<div className="skill-name-percent">
+							<h2>SpringBoot</h2>
+							<h6>70%</h6>
+						</div>
+						<div class="progress-bar">
+							<span class="percentage spring"></span>
+						</div>
+					</div>
+
+					<div class="progress-bar-container">
+						<div className="skill-name-percent">
+							<h2>MySql</h2>
+							<h6>90%</h6>
+						</div>
+						<div class="progress-bar">
+							<span class="percentage sql"></span>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
